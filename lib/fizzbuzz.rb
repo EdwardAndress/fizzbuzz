7e6d7fc66aae7ae9fe711268d63f575d688aa3f7
edward andress <eddie_andress@hotmail.com>
2014-11-02 13:02:40 +0000

    refactored and brought together methods so only one method call is made by the user

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,11 +1,18 @@
 def is_divisible_by_three(num)
-	num % 3 == 0 ? 'Fizz' : num
+	num % 3 == 0
 end
 
 def is_divisible_by_five(num)
-	num % 5 == 0 ? 'Buzz' : num
+	num % 5 == 0
 end
 
 def is_divisible_by_fifteen(num)
-	num % 15 == 0 ? 'FizzBuzz' : num
+	num % 15 == 0
+end
+
+def fizzbuzz(num)
+	return 'FizzBuzz' if is_divisible_by_fifteen(num)
+	return 'Fizz' if is_divisible_by_three(num)
+	return 'Buzz' if is_divisible_by_five(num)
+	return num
 end
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -5,21 +5,15 @@ describe 'fizzbuzz' do
 	context 'FizzBuzz should accept a number and return' do
 
 		it 'Fizz when the number is a mulitple of 3' do
-			expect(is_divisible_by_three(3)).to eq 'Fizz'
+			expect(is_divisible_by_three(3)).to eq true
 		end
 
 		it 'Buzz when the number is divisible by 5' do
-			expect(is_divisible_by_five(5)).to eq 'Buzz'
+			expect(is_divisible_by_five(5)).to eq true
 		end
 
 		it 'FizzBuzz when the number is divisible by both 3 and 5' do
-			expect(is_divisible_by_fifteen(15)).to eq 'FizzBuzz'
-		end
-
-		it 'the number itself, if it is not divisible by 3 or 5' do
-			expect(is_divisible_by_fifteen(3)).to eq 3
-			expect(is_divisible_by_three(5)).to eq 5
-			expect(is_divisible_by_five(3)).to eq 3
+			expect(is_divisible_by_fifteen(15)).to eq true
 		end
 
 	end
@@ -28,6 +22,9 @@ describe 'fizzbuzz' do
 
 		it 'when provided individial numbers' do
 			expect(fizzbuzz(3)).to eq 'Fizz'
+			expect(fizzbuzz(5)).to eq 'Buzz'
+			expect(fizzbuzz(15)).to eq 'FizzBuzz'
+			expect(fizzbuzz(1)).to eq 1
 		end
 
 	end
